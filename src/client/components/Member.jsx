70992173a8f402f7c84ae72213474e756377e316
progfay <shun1856shun@gmail.com>
2019-02-11 18:42:23 +0900

    feat(Member): add showing Avatar and display name component

---
+++ src/client/components/Member.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Flex, Link, Avatar, Tooltip } from '@primer/components'
+
+const Member = ({ user }) => {
+  return (
+    <Flex alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+      <Tooltip text={user.displayName}>
+        <Link href={`https://github.com/${user.displayName}`} target='_blank' rel='noopener noreferrer'>
+          <Avatar src={user.photoURL} size={64} />
+        </Link>
+      </Tooltip>
+    </Flex>
+  )
+}
+
+export default Member
