c51ae171945b9ac6bab051867c5129f2e3e9ea87
progfay <shun1856shun@gmail.com>
2019-02-14 03:48:33 +0900

    feat: add Member List Component

---
+++ src/client/components/Members.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { Flex } from '@primer/components'
+import Member from '../components/Member'
+import firebase from '../lib/firebase'
+
+const Members = () => {
+  const [ users, setUsers ] = useState([])
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user || users.length > 0) return
+      setUsers([ ...users, user, user, user ])
+    })
+    return unsubscribe
+  })
+  return (
+    <Flex m={5} alignItems={'center'} justifyContent={'space-evenly'}>
+      {users.map(user => (
+        <Flex.Item key={user.uid}>
+          <Member user={user} />
+        </Flex.Item>
+      ))}
+    </Flex>
+  )
+}
+
+export default Members
