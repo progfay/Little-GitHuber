72f4ec334151b8fe2fdbefee6433dffe2d7cea38
progfay <shun1856shun@gmail.com>
2019-02-14 03:49:03 +0900

    feat: support react-router-dom with express

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'), (err) => { if (err) res.status(500).send(err) })
+})
 
 app.listen(8080, () => console.log('Listening on port 8080!'))
