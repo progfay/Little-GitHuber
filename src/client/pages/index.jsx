f7089aed0a920684871b91459c6e77b7266bce81
progfay <shun1856shun@gmail.com>
2019-02-14 03:50:10 +0900

    feat: route as SPA with react-router

---
+++ src/client/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Auth from '../components/Auth'
+import Members from '../components/Members'
+
+const App = ({ history }) => (
+  <Router history={history}>
+    <>
+      <Route path='/auth' component={Auth} />
+      <Route path='/*' component={Members} />
+    </>
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
