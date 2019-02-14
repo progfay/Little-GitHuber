fd62f31429a707dcaf0b8e53f0be8f63d2d37768
progfay <shun1856shun@gmail.com>
2019-02-15 02:58:18 +0900

    feat(routing): show only one component

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Auth from './components/Auth'
 import Members from './components/Members'
 
 const App = ({ history }) => (
   <Router history={history}>
-    <>
+    <Switch>
       <Route path='/auth' component={Auth} />
       <Route path='/*' component={Members} />
-    </>
+    </Switch>
   </Router>
 )
 
