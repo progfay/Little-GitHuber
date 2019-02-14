125fbdfa9212d29939fdf645022ef4e734f82df0
progfay <shun1856shun@gmail.com>
2019-02-15 04:10:25 +0900

    feat(/user): add user data from uid

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,14 @@ const app = express()
 
 app.use(express.static('dist'))
 
+const firebase = require('./firebaseAdmin')
+app.get('/user', (req, res) => {
+  if (!req.query.uid) res.status(404).send('uid not set.')
+  firebase.auth().getUser(req.query.uid)
+    .then(user => { res.send(user.toJSON()) })
+    .catch(error => { res.status(500).send(error) })
+})
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'), (err) => { if (err) res.status(500).send(err) })
 })
