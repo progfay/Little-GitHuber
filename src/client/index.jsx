f3abfc5958ebfb61e820136b8ac4222f5a98a1dc
progfay <shun1856shun@gmail.com>
2019-02-15 04:12:31 +0900

    feat: update routing with useing Cookies

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Auth from './components/Auth'
-import Members from './components/Members'
+import { CookiesProvider } from 'react-cookie'
+import Members from './pages/Members'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
 
 const App = ({ history }) => (
-  <Router history={history}>
-    <Switch>
-      <Route path='/auth' component={Auth} />
-      <Route path='/*' component={Members} />
-    </Switch>
-  </Router>
+  <CookiesProvider>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/' component={Members} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/logout' component={Logout} />
+      </Switch>
+    </Router>
+  </CookiesProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
