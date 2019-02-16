44d93ab29d7b22617103f33ea58a6db117862383
progfay <shun1856shun@gmail.com>
2019-02-17 03:02:17 +0900

    feat: get members from server

--- src/client/pages/Members.jsx
+++ src/client/pages/Members.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react'
+/* global fetch */
+
+import React, { useState } from 'react'
 import { withCookies } from 'react-cookie'
 import { Flex } from '@primer/components'
 import Member from '../components/Member'
-import firebase from '../lib/firebase'
+// import firebase from '../lib/firebase'
 
 const Members = () => {
-  const [ users, setUsers ] = useState([])
-  useEffect(() => {
-    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (!user || users.length > 0) return
-      setUsers([ ...users, user, user, user ])
-    })
-    return unsubscribe
-  })
+  const [ users, setUsers ] = useState(null)
+
+  if (!users) {
+    fetch('/users')
+      .then(response => response.json())
+      .then(setUsers)
+    return null
+  }
+
   return (
-    <Flex m={5} alignItems={'center'} justifyContent={'space-evenly'}>
+    <Flex m={5} alignItems={'center'} justifyContent={'space-evenly'} flexWrap={'wrap'}>
       {users.map(user => (
         <Flex.Item key={user.uid}>
           <Member user={user} />
