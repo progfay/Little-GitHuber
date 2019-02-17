2150b0fc67319840791aca5b33280f66d07e7635
progfay <shun1856shun@gmail.com>
2019-02-17 18:09:22 +0900

    feat(/): show group list page

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
-import Members from './pages/Members'
 import Login from './pages/Login'
 import Logout from './pages/Logout'
+import GroupList from './pages/GroupList'
 
 const App = ({ history }) => (
   <CookiesProvider>
     <Router history={history}>
       <Switch>
-        <Route exact path='/' component={Members} />
+        <Route exact path='/' component={GroupList} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/logout' component={Logout} />
       </Switch>
---
+++ src/client/pages/GroupList.jsx
@@ -0,0 +1,30 @@
+/* global fetch */
+
+import React, { useState } from 'react'
+import { withCookies } from 'react-cookie'
+import { Flex } from '@primer/components'
+import Group from '../components/Group'
+// import firebase from '../lib/firebase'
+
+const Members = () => {
+  const [ groupList, setGroupList ] = useState(null)
+
+  if (!groupList) {
+    fetch('/group-list')
+      .then(response => response.json())
+      .then(setGroupList)
+    return null
+  }
+
+  return (
+    <Flex m={5} alignItems={'center'} justifyContent={'space-evenly'} flexWrap={'wrap'}>
+      {groupList.map((members, index) => (
+        <Flex.Item key={index}>
+          <Group members={members} branchName={'refactor-hogefuga'} />
+        </Flex.Item>
+      ))}
+    </Flex>
+  )
+}
+
+export default withCookies(Members)
